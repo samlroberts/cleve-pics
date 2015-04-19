a8b6778ac2f05f4b94667d2f02685880fd2d60e3
Sam Roberts <samlroberts+github@gmail.com>
2015-04-19 14:44:47 -0400

    render images

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ var app = express();
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var testImages = [ {url:'http://pbs.twimg.com/media/CC5xUEgUIAEjAo3.jpg'},
+  {url:'http://pbs.twimg.com/media/CC5MogZW4AAZfmu.jpg'},
+  {url:'http://pbs.twimg.com/media/CC5xUEgUIAEjAo3.jpg'},
+  {url:'http://pbs.twimg.com/media/CC6ARAoW0AADQXE.jpg'},
+  {url:'http://pbs.twimg.com/media/CC5xUEgUIAEjAo3.jpg'},
+  {url:'http://pbs.twimg.com/media/CC5MogZW4AAZfmu.jpg'},
+  {url:'http://pbs.twimg.com/media/CC5m4oTWIAEm0kf.jpg'} ]
+
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home',{images:testImages});
 });
  
 app.listen(3000)
