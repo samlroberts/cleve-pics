fb7679282e47b197794fd371376a3e0eea209457
Sam Roberts <samlroberts+github@gmail.com>
2015-04-19 14:17:01 -0400

    get authentication working; save test images

--- twitter.js
+++ twitter.js
@@ -1,12 +1,42 @@
 var request = require('request')
 var twitter_api = 'https://api.twitter.com/1.1/';
-var consumer_key    = '0KDTbTS5fvEZGorYg5kPoCNeL';
-var consumer_secret = 'YuS1QJiS4sRfYQbfbgr1jxHmXq8awcnY8Rf0H5NVr3kE45FACF';
+var consumer_key        = 'Pa1IpwxdqsAO6PNEHXceY1oib';
+var consumer_secret     = 'Dw9XzqKBk7jh1baqVTr6LzsqeV3oKl0dTWFgQERMIp8okNXC1V';
+var access_token = 'AAAAAAAAAAAAAAAAAAAAAG07fQAAAAAABuhm%2FwUfhCgV%2FbykVEqEhDTtdIc%3DanUC7ZRsGwpGbZNaiEeZ75DiWPMWIuzScW7A6xqvIoHdy1IYqN';
+// var access_token_secret = 'jvgUgHf7kbqu4qNyITqi57qGcdspb1HSGdCuNl66plEh7';
+
+
+// var auth = consumer_key + ":" + consumer_secret;
+// console.log(auth);
+// auth = new Buffer(auth).toString("base64");
+// console.log(auth);
+
+// var requestObject = {
+// 	url:'https://api.twitter.com/oauth2/token',
+// 	method: 'POST',
+// 	headers:{
+// 		'Authorization': 'Basic '+auth,
+// 		'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+// 	},
+// 	body:'grant_type=client_credentials'
+// 	}
+
+// console.log(requestObject);
+// request(requestObject,function(error,response,body){
+// 	if (!error && response.statusCode == 200) {
+// 		var data = JSON.parse(body);
+// 		console.log(data);
+// 		access_token = data.access_token
+// 		request(options, callback);
+// 	} else {
+// 		console.log(response);
+// 	}
+// });
 
 var options = {
   url: 'https://api.twitter.com/1.1/search/tweets.json?q=%23happyincle&src=typd&mode=photos',
   headers: {
-    Authorization: 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1429397662",oauth_nonce="1600163735",oauth_version="1.0",oauth_token="7956002-GZBErfwi0j70kxLqZ6XSTZ8pPesuuhwlYJDRO52oAu",oauth_signature="fO12lS%2BCRlRU6FUSQk5H859imDE%3D"',
+    Authorization: 'Bearer '+access_token,
     Host:'api.twitter.com',
     'X-Target-URI':'https://api.twitter.com',
     Connection:'Keep-Alive'
@@ -24,10 +54,19 @@ function callback(error, response, body) {
 				output.push(info.statuses[i].entities.media[0].media_url)
 			}
 		}
-		console.log(output.length)
+		console.log(output)
 	} else {
 		console.log(response)
 	}
 }
  
-request(options, callback);
+
+// request(options,callback)
+
+var testImages = [ 'http://pbs.twimg.com/media/CC5xUEgUIAEjAo3.jpg',
+  'http://pbs.twimg.com/media/CC5MogZW4AAZfmu.jpg',
+  'http://pbs.twimg.com/media/CC5xUEgUIAEjAo3.jpg',
+  'http://pbs.twimg.com/media/CC6ARAoW0AADQXE.jpg',
+  'http://pbs.twimg.com/media/CC5xUEgUIAEjAo3.jpg',
+  'http://pbs.twimg.com/media/CC5MogZW4AAZfmu.jpg',
+  'http://pbs.twimg.com/media/CC5m4oTWIAEm0kf.jpg' ]
