9dfef0b0f7cd988208a10a05702b52f07a335217
Sam Roberts <samlroberts+github@gmail.com>
2015-05-02 17:57:15 -0400

    make port depend on env variable

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('/', function (req, res) {
 	});
 });
  
-app.listen(3000)
-console.log('listening on port 3000')
+app.listen(process.env.PORT || 8080)
+console.log('listening on port '+ (process.env.PORT || 8080));
